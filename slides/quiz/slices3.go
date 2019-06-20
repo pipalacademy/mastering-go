48317f7e3fbba84e1029633b71905fbcef4f0e6f
Devi Adivi <asldevi@gmail.com>
2019-06-20 16:31:31 +0530

    add more to slices quiz

--- slides/quiz/slices3.go
+++ slides/quiz/slices3.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func f(s []int) {
-	s = []int{1, 2, 3}
+	s = []int{4, 5, 6}
 }
 
 func g(s *[]int) {
 	*s = []int{1, 2, 3}
+}
+
+func h(s []int) {
+	s[0] = 100
 }
 
 func main() {
@@ -15,5 +19,7 @@ func main() {
 	f(s)
 	fmt.Println(s, cap(s), len(s))
 	g(&s)
+	fmt.Println(s, cap(s), len(s))
+	h(s)
 	fmt.Println(s, cap(s), len(s))
 }
